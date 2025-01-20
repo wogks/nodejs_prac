fb50f525b5be6769f669a7db3ef9c2b9c0edda24
wogks <108786958+wogks@users.noreply.github.com>
2025-01-20 23:29:05 +0900

    Feat: making router

--- src/server.js
+++ src/server.js
@@ -1,10 +1,41 @@
 import express from "express";
+import morgan from "morgan";
+
+const PORT = 4001;
 
 const app = express();
+const logger = morgan("dev");
+app.use(logger);
 
-const PORT = 4000;
+const globalRouter = express.Router();
 
-const handleListening = () => console.log(`server listening http:locahost:${PORT}`);
+const handleHome = (req, res) =>
+    res.send("Home");
+
+globalRouter.get("/", handleHome);
+
+const userRouter = express.Router();
+
+const handleUserEditor = (req, res) => res.send("Edit user");
+
+userRouter.get("/edit", handleUserEditor)
+
+const videoRouter = express.Router();
+
+const handleWatchVideo = (req, res) => res.send("watch video");
+
+videoRouter.get("/watch", handleWatchVideo);
+
+app.use("/", globalRouter);
+app.use("/videos", videoRouter);
+app.use("/users", userRouter);
+
+
+
+
+
+
+const handleListening = () => console.log(`server listening http://localhost:${PORT}`);
 
 
 app.listen(PORT, handleListening);
